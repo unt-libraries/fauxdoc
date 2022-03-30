caf2f979c9d2913e0c9adcb88ae182fb70bf79c8
Jason Thomale <jason.thomale@unt.edu>
2022-03-30 17:15:15 -0500

    Improve type-checking for emitters
    This sets up a more systematic way to do type-checking to ensure (or
    reasonably ensure) an object matches the appropriate emitter-type
    interface.

--- src/solrfixtures/profile.py
+++ src/solrfixtures/profile.py
@@ -4,7 +4,7 @@ Contains classes for creating faux-data-generation profiles.
 from collections import OrderedDict
 from typing import Any, Callable, Optional, Union
 
-from solrfixtures.emitter import check_emitter, StaticEmitter
+from solrfixtures.emitter import Emitter, StaticEmitter
 from solrfixtures.emitters.choice import Choice
 from solrfixtures.typing import EmitterLike, IntEmitterLike, T
 
@@ -31,34 +31,52 @@ class Field:
 
     def _init_emitter(self, emitter: EmitterLike) -> None:
         try:
-            self.emitter = check_emitter(emitter)
+            self.emitter = Emitter.check_object(emitter)
         except TypeError as e:
-            raise TypeError(f"Incorrect type for 'emitter' attribute. {e}")
+            raise TypeError(f"Incorrect type for 'emitter' attribute.") from e
 
     def _init_repeat_emitter(self, repeat: Optional[IntOrIntEmitter]) -> None:
+        err_msg = (
+            f"Incorrect type for 'repeat' argument. Expected an `int` or a "
+            f"callable {Emitter.interface_name}-like that emits `int`s"
+        )
         if callable(repeat):
             try:
-                self.repeat_emitter = check_emitter(repeat)
+                self.repeat_emitter = Emitter.check_object(repeat, (int,))
             except TypeError as e:
                 raise TypeError(
-                    f"Incorrect type for 'repeat' argument. A callable was "
-                    f"provided, but it should be Emitter-like. {e}"
-                )
+                    f'{err_msg}; got a callable object that failed further '
+                    f'type checks.'
+                ) from e
         else:
+            if repeat is not None and not isinstance(repeat, int):
+                raise TypeError(f'{err_msg}, but got a `{type(repeat)}`.')
             self.repeat_emitter = StaticEmitter(repeat)
 
     def _init_gate_emitter(self, chance: Optional[IntOrIntEmitter]) -> None:
+        err_msg = (
+            f"Incorrect type for 'chance' argument. Expected an `int` between "
+            f"0 and 100 or a callable {Emitter.interface_name}-like that "
+            "emits `bool`s"
+        )
         if callable(chance):
             try:
-                self.gate_emitter = check_emitter(chance)
+                self.gate_emitter = Emitter.check_object(chance, (bool,))
             except TypeError as e:
                 raise TypeError(
-                    f"Incorrect type for 'chance' argument. A callable was "
-                    f"provided, but it should be Emitter-like. {e}"
-                )
+                    f'{err_msg}; got a callable object that failed further '
+                    f'type checks.'
+                ) from e
         elif chance is None:
             self.gate_emitter = StaticEmitter(True)
         else:
+            if not isinstance(chance, int):
+                raise TypeError(f'{err_msg}, but got a `{type(chance)}`.')
+            if not 0 <= chance <= 100:
+                raise ValueError(
+                    f"Incorrect 'chance' argument. `int` values must be 0 to "
+                    f"100; got {chance}."
+                )
             self.gate_emitter = Choice([True, False], [chance, 100 - chance])
 
     def reset(self) -> None:
