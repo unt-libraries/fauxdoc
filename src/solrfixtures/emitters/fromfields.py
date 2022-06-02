14c191ac635ef854fe7f84bca0d23e823c4f9bee
Jason Thomale <jason.thomale@unt.edu>
2022-06-02 12:05:57 -0500

    Add emitters.fromfields.BasedOnFields
    This adds a `BasedOnFields` emitter type, a companion to `CopyFields`
    that lets you emit some value or values that are *based on* the values
    output by the source field or fields. The user supplies the list of
    source fields and an 'action' function that receives the source output
    and does something with it to produce the final output.
    
    Note that the same results were already possible using a wrapper from
    `emitters.wrappers` to wrap a CopyFields emitter. However, the
    BasedOnFields emitter is faster, particularly when you need to source
    more than one field. Plus it has built-in handling for RNG that's
    consistent with other RNG handling, which the wrappers currently lack.

--- src/solrfixtures/emitters/fromfields.py
+++ src/solrfixtures/emitters/fromfields.py
@@ -1,8 +1,10 @@
 """Contains emitters that use Field data to generate output."""
-from typing import List, Optional, Sequence, Union
+import random
+from typing import Any, Callable, List, Optional, Sequence, Union
 
 from solrfixtures.group import ObjectGroup
 from solrfixtures.emitter import Emitter
+from solrfixtures.mixins import RandomMixin
 from solrfixtures.typing import FieldLike, T
 
 
@@ -21,8 +23,9 @@ class CopyFields(Emitter):
           fields into a single string value, joined using a provided
           'separator'.
 
-    If you need anything more complicated, you'll have to create a
-    custom emitter.
+    Use a BasedOnFields emitter instead if you need to modify the
+    values from existing fields or otherwise base your output on
+    existing fields rather than copy it exactly.
 
     Note that, in your schema assignment, you should put each Field
     using this emitter AFTER all the fields it copies. E.g., the value
@@ -72,6 +75,22 @@ class CopyFields(Emitter):
             self._source = ObjectGroup(source)
             self._single_valued = not source.multi_valued
 
+    def reset_source(self) -> None:
+        """Calls `reset` on all source fields.
+
+        Note that calling `self.reset` does not automatically reset
+        source fields because that behavior would generally be
+        redundant. I.e., usually a CopyField instance and its source
+        Field instances will belong to the same Schema instance, and
+        when used in that context, calling the Schema's `reset_fields`
+        method resets all source fields anyway. So with the primary use
+        case, reseting sources when `self.reset` is called would reset
+        them twice. This method is provided for unusual or advanced use
+        cases where you're using a BasedOnField instance outside a
+        schema, or your source fields aren't part of the same schema.
+        """
+        self._source.do_method('reset')
+
     def emit(self) -> T:
         """Returns one emitted value."""
         if self._single_valued:
@@ -94,3 +113,92 @@ class CopyFields(Emitter):
             number: See superclass.
         """
         return [self.emit()] * number
+
+
+class BasedOnFields(RandomMixin, CopyFields):
+    """Emitter class for basing output on existing Field instances.
+
+    Use this if you need to emit data that is based on the data from
+    other fields -- like CopyFields, but you supply an 'action'
+    function that modifies the value(s) before returning anything.
+
+    Like with CopyFields, the 'source' you supply may be a single field
+    or a sequence of fields. If it's a single field, then the 'action'
+    callable receives the raw data value from that field. If it's a
+    sequence of fields, the 'action' callable receives a dictionary
+    that maps source fields to their output values, using field names
+    as keys.
+
+    Note that this is faster than using wrapper emitters to wrap
+    CopyFields output, although functionally equivalent.
+
+    Also note that, in your schema assignment, you should put each
+    Field using this emitter AFTER all of its source fields. E.g., the
+    values being sourced won't exist until the source fields emit data.
+
+    Attributes:
+        source: An ObjectGroup containing the Field instance(s) to copy
+            data from. Values are sent to the 'action' function in
+            Field order.
+        action: A callable that takes either one or two args. The first
+            is always the source data. The second is the 'rng'
+            (random.Random) instance from the BasedOnFields instance,
+            provided only if 'needs_rng' is True. It should return the
+            appropriate output value or values.
+        needs_rng: (Optional.) A boolean value that dictates whether or
+            not the second (rng) argument will be supplied to the
+            'action' callable when called. Default is False.
+        rng_seed: (Optional.) See parent class (RandomMixin).
+    """
+
+    def __init__(self,
+                 source: Union[FieldLike, Sequence[FieldLike]],
+                 action: Union[Callable[[Any], Any],
+                               Callable[[Any, random.Random], Any]],
+                 needs_rng: bool = False,
+                 rng_seed: Any = None) -> None:
+        """Inits a BasedOnFields instance.
+
+        Args:
+            source: Provide either one Field instance or a sequence of
+                Field instances (to get base data values from).
+            action: See `action` attribute.
+            needs_rng: (Optional.) See `needs_rng` attribute.
+            rng_seed: (Optional.) See `rng_seed` attribute.
+        """
+        super().__init__(source, rng_seed=rng_seed)
+        self.action = action
+        self.needs_rng = needs_rng
+
+    def seed_source(self, rng_seed: Any) -> None:
+        """Seeds all RNGs associated with source fields.
+
+        Note that calling `self.seed` does not automatically reseed
+        source fields because that behavior would generally be
+        redundant. I.e., usually a BasedOnField instance and its source
+        Field instances will belong to the same Schema instance, and
+        when used in that context, calling the Schema's `seed_fields`
+        method seeds all source fields anyway. So with the primary use
+        case, seeding sources when `self.seed` is called would seed
+        them twice. This method is provided for unusual or advanced use
+        cases where you're using a BasedOnField instance outside a
+        schema, or your source fields aren't part of the same schema.
+
+        Args:
+            rng_seed: The new seed you want to set. Ultimately this is
+                passed to a random.Random instance, so it should be any
+                value valid for seeding random.Random.
+        """
+        self._source.do_method('seed', rng_seed)
+
+    def emit(self) -> T:
+        """Returns one emitted value."""
+        if len(self._source) == 1:
+            vals = self._source[0].previous
+        else:
+            vals = {}
+            for field in self._source:
+                vals[field.name] = field.previous
+        if self.needs_rng:
+            return self.action(vals, self.rng)
+        return self.action(vals)
--- tests/test_emitters_fromfields.py
+++ tests/test_emitters_fromfields.py
@@ -2,10 +2,61 @@
 import pytest
 
 from solrfixtures.emitters.fixed import Static
-from solrfixtures.emitters.choice import chance
-from solrfixtures.emitters.fromfields import CopyFields
+from solrfixtures.emitters.choice import chance, Choice
+from solrfixtures.emitters.fromfields import CopyFields, BasedOnFields
 from solrfixtures.profile import Field
 
+
+# Fixtures / test data
+
+def single_field_multi(values):
+    if values is not None:
+        return [str(v) for v in values]
+
+
+def single_field_multi_collapse(values):
+    if values is not None:
+        return ' '.join([str(v) for v in values])
+
+
+def single_field_random(value, rng):
+    em = chance(0.5)
+    em.rng = rng
+    return str(value) if em() else value
+
+
+def multi_field_each_single(data):
+    return [str(v) for v in data.values() if v is not None] or None
+
+
+def multi_field_each_multi(data):
+    return [str(x) for v in data.values() if v is not None for x in v] or None
+
+
+def multi_field_each_single_collapse(data):
+    return ' '.join([str(v) for v in data.values() if v is not None]) or None
+
+
+def multi_field_each_multi_collapse(data):
+    return ' '.join([
+        str(x) for v in data.values() if v is not None for x in v
+    ]) or None
+
+
+def multi_field_random(data, rng):
+    em = chance(0.5)
+    em.rng = rng
+    render_stack = []
+    for vals in data.values():
+        for val in vals:
+            if em():
+                render_stack.append(f'{val}!')
+            else:
+                render_stack.append(str(val))
+    return ' '.join(render_stack)
+
+
+# Tests
 
 @pytest.mark.parametrize('source, separator, expected', [
     # Single-field tests
@@ -59,6 +110,75 @@ from solrfixtures.profile import Field
 ])
 def test_copyfields_emit(source, separator, expected):
     em = CopyFields(source, separator)
-    [field() for field in em.source]
+
+    # load initial field data
+    _ = [field() for field in em.source]
     assert em() == expected
+    assert em(2) == [expected, expected]
+
+
+@pytest.mark.parametrize('source, action, needs_rng, seed, expected', [
+    # Single-field tests
+    (Field('test1', Static(0)), str, False, None, '0'),
+    (Field('test1', Static(0), repeat=Static(1)), single_field_multi,
+     False, None, ['0']),
+    (Field('test1', Static(0), repeat=Static(1)), single_field_multi_collapse,
+     False, None, '0'),
+    (Field('test1', Static(0), repeat=Static(3)), single_field_multi,
+     False, None, ['0', '0', '0']),
+    (Field('test1', Static(0), repeat=Static(3)), single_field_multi_collapse,
+     False, None, '0 0 0'),
+    (Field('test1', Static(0), repeat=Static(3), gate=chance(0)),
+     single_field_multi_collapse, False, None, None),
+    (Field('test1', Choice(range(1, 6))), single_field_random, True, 999,
+     1),
+
+    # Multi-field tests
+    ([Field('test1', Static(1)),
+      Field('test2', Static(2)),
+      Field('test3', Static(3))], multi_field_each_single, False, None,
+     ['1', '2', '3']),
+    ([Field('test1', Static(1)),
+      Field('test2', Static(2)),
+      Field('test3', Static(3))], multi_field_each_single_collapse, False,
+     None, '1 2 3'),
+    ([Field('test1', Static(1), repeat=Static(1)),
+      Field('test2', Static(2), repeat=Static(2)),
+      Field('test3', Static(3), repeat=Static(2))], multi_field_each_multi,
+     False, None, ['1', '2', '2', '3', '3']),
+    ([Field('test1', Static(1), repeat=Static(1)),
+      Field('test2', Static(2), repeat=Static(2)),
+      Field('test3', Static(3), repeat=Static(2))],
+     multi_field_each_multi_collapse, False, None, '1 2 2 3 3'),
+    ([Field('test1', Static(1), repeat=Static(1), gate=chance(0)),
+      Field('test2', Static(2), repeat=Static(2), gate=chance(0)),
+      Field('test3', Static(3), repeat=Static(2))], multi_field_each_multi,
+     False, None, ['3', '3']),
+    ([Field('test1', Static(1), repeat=Static(1), gate=chance(0)),
+      Field('test2', Static(2), repeat=Static(2), gate=chance(0)),
+      Field('test3', Static(3), repeat=Static(2), gate=chance(0))],
+     multi_field_each_multi, False, None, None),
+    ([Field('test1', Choice(range(1, 6)), repeat=Choice(range(1, 4))),
+      Field('test2', Static('-'), repeat=Choice(range(1, 4))),
+      Field('test3', Choice('ABCDEFG'), repeat=Static(2))], multi_field_random,
+     True, 999, '4 1! 5 - -! -! F A!'),
+    ([Field('test1', Static('1'), repeat=Static(2)),
+      Field('test2', Static('2')),
+      Field('test3', Static('3'))],
+     lambda v: ':'.join([' '.join(v['test1']), v['test2'], v['test3']]), False,
+     None, '1 1:2:3'),
+])
+def test_basedonfields_emit(source, action, needs_rng, seed, expected):
+    em = BasedOnFields(source, action, needs_rng, seed)
+
+    # Note: Normally the owning schema instance will handle seeding and
+    # loading the source fields. Because we are testing outside the
+    # context of a schema instance, we have to do this manually.
+    em.seed_source(seed)
+    _ = [field() for field in em.source]
+    assert em() == expected
+
+    em.reset_source()
+    em.reset()
+    _ = [field() for field in em.source]
     assert em(2) == [expected, expected]
